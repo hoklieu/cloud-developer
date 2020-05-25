201595306c12d6d54c26c1e683f8fe91f68bdbef
hoklieu <song51689@gmail.com>
2020-05-24 22:01:11 -0400

    exercise for adding some endpoints for backend

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -2,6 +2,7 @@ import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 import { Car, cars as cars_list } from './cars';
+import { expandCars } from "./utils";
 
 (async () => {
   let cars:Car[]  = cars_list;
@@ -29,12 +30,12 @@ import { Car, cars as cars_list } from './cars';
 
       if ( !name ) {
         return res.status(400)
-                  .send(`name is required`);
+        .send(`name is required`);
       }
 
       return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+      .send(`Welcome to the Cloud, ${name}!`);
+    } );
 
   // Get a greeting to a specific person to demonstrate req.query
   // > try it {{host}}/persons?name=the_name
@@ -43,11 +44,11 @@ import { Car, cars as cars_list } from './cars';
 
     if ( !name ) {
       return res.status(400)
-                .send(`name is required`);
+      .send(`name is required`);
     }
 
     return res.status(200)
-              .send(`Welcome to the Cloud, ${name}!`);
+    .send(`Welcome to the Cloud, ${name}!`);
   } );
 
   // Post a greeting to a specific person
@@ -61,26 +62,79 @@ import { Car, cars as cars_list } from './cars';
 
       if ( !name ) {
         return res.status(400)
-                  .send(`name is required`);
+        .send(`name is required`);
       }
 
       return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+      .send(`Welcome to the Cloud, ${name}!`);
+    });
 
   // @TODO Add an endpoint to GET a list of cars
-  // it should be filterable by make with a query paramater
-
+  app.get("/cars",
+    (req: Request, res: Response) => {
+      let { make } = req.query;
+      if (make) {
+        console.log("query");
+        let result = cars.filter(function(car) {
+          return car.make == make;
+        });
+        return res.status(200)
+        .send(result);
+      }
+      return res.status(200)
+      .send(cars);
+    });
   // @TODO Add an endpoint to get a specific car
-  // it should require id
   // it should fail gracefully if no matching car is found
+  app.get("/cars/:id",
+    (req: Request, resp: Response) => {
+      let { id } = req.params;
+      if (id) {
+        let result = cars.filter((car) => car.id == id);
+        // have to do it this way... empty array still exists
+        if (result && result.length != 0)
+        {
+          return resp.status(200)
+          .send(result);
+        }
+        else
+        {
+          return resp.status(404)
+          .send(`${id} not found! {result}` );
+        }
+
+      }
+    })
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+  app.post("/cars", 
+    async (req : Request, resp: Response) => {
+      const { make, id, type, model, cost } = req.body;
+      console.log(make, id, type, model, cost);
+      if (make && id && type && model && cost)
+      {
+        if (id <= cars.length)
+        {
+          return resp.status(404)
+          .send("You're missing fields"); 
+        }
+        const new_car: Car = 
+        {"make": make, "id": id, "type": type, "model":model, "cost":cost};
+        cars.push(new_car);
+        return resp.status(201)
+        .send(new_car);
+      }
+      else
+      {
+        return resp.status(404)
+        .send("You're missing fields");
+      }
+    });
 
   // Start the Server
   app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
+    console.log( `server running http://localhost:${ port }` );
+    console.log( `press CTRL+C to stop server` );
   } );
 })();
